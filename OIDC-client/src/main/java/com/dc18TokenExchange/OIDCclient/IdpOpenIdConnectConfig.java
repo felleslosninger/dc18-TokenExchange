cf6b080972fd7aa05536067638a0cd29c2755b90
Peter Rydberg <hmp.rydberg@gmail.com>
2018-06-20 15:58:08 +0200

    Tested idpConnect

---
+++ OIDC-client/src/main/java/com/dc18TokenExchange/OIDCclient/IdpOpenIdConnectConfig.java
@@ -0,0 +1,53 @@
+package com.dc18TokenExchange.OIDCclient;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableOAuth2Client
+public class IdpOpenIdConnectConfig {
+
+    @Value("${idp.clientId}")
+    private String clientId;
+
+    @Value("${idp.clientSecret}")
+    private String clientSecret;
+
+    @Value("${idp.accessTokenUri}")
+    private String accessTokenUri;
+
+    @Value("${idp.userAuthorizationUri}")
+    private String userAuthorizationUri;
+
+    @Value("${idp.redirectUri}")
+    private String redirectUri;
+
+    @Bean
+    public OAuth2ProtectedResourceDetails idpOpenId() {
+        AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
+        details.setAccessTokenUri(accessTokenUri);
+        details.setUserAuthorizationUri(userAuthorizationUri);
+        details.setScope(Arrays.asList("openid", "email"));
+        details.setPreEstablishedRedirectUri(redirectUri);
+        details.setUseCurrentUri(false);
+        return details;
+    }
+
+    @Bean
+    public OAuth2RestTemplate idpOpenIdTemplate(OAuth2ClientContext clientContext) {
+        return new OAuth2RestTemplate(idpOpenId(), clientContext);
+    }
+}
+
---
+++ OIDC-client/src/main/java/com/dc18TokenExchange/OIDCclient/OpenIdConnectFilter.java
@@ -0,0 +1,49 @@
+package com.dc18TokenExchange.OIDCclient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class OpenIdConnectFilter extends AbstractAuthenticationProcessingFilter {
+
+    public OpenIdConnectFilter(String defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+        setAuthenticationManager(new NoopAuthenticationManager());
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        OAuth2AccessToken accessToken;
+        try {
+            accessToken = restTemplate.getAccessToken();
+        } catch (OAuth2Exception e) {
+            throw new BadCredentialsException("Could not obtain access token", e);
+        }
+        try {
+            String idToken = accessToken.getAdditionalInformation().get("id_token").toString();
+            String kid = JwtHelper.headers(idToken).get("kid");
+            Jwt tokenDecoded = JwtHelper.decodeAndVerify(idToken, verifier(kid));
+            Map<String, String> authInfo = new ObjectMapper()
+                    .readValue(tokenDecoded.getClaims(), Map.class);
+            verifyClaims(authInfo);
+            OpenIdConnectUserDetails user = new OpenIdConnectUserDetails(authInfo, accessToken);
+            return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        } catch (InvalidTokenException e) {
+            throw new BadCredentialsException("Could not obtain user details from token", e);
+        }
+    }
+}
