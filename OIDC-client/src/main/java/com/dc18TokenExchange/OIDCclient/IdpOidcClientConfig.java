62de2bd367ce04ecc66d2bbb6c292ae780675e7a
Eidissen <camp-hei@DIFI.no>
2018-06-27 12:06:32 +0200

    med litt mer kode fra difi

---
+++ OIDC-client/src/main/java/com/dc18TokenExchange/OIDCclient/IdpOidcClientConfig.java
@@ -0,0 +1,32 @@
+package com.dc18TokenExchange.OIDCclient;
+
+import lombok.Getter;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class IdpOidcClientConfig implements InitializingBean {
+
+    private String clientId = "oidc_dificamp_test";
+    private String clientSecret = "a865d76b-0bb2-45b4-92a0-109767da5c7d";
+    private String redirectUri = "http://localhost:8000/welcome";
+
+    private String postLogoutRedirectUri;
+    private String logoutUri;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logoutUri = redirectUri.replace("welcome", "logut");
+        postLogoutRedirectUri = logoutUri + "/response";
+    }
+
+    public String getClientId(){
+        return clientId;
+    }
+
+    public String getRedirectUri(){
+        return redirectUri;
+    }
+}
+
