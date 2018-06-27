a0de0910f8bef591c8e731b1dc848a2a3b1dded3
Peter Rydberg <hmp.rydberg@gmail.com>
2018-06-27 14:02:33 +0200

    Mer database

--- STS-server/src/main/java/com/dc18TokenExchange/STSserver/StsServerApplication.java
+++ STS-server/src/main/java/com/dc18TokenExchange/STSserver/StsServerApplication.java
@@ -2,8 +2,10 @@ package com.dc18TokenExchange.STSserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StsServerApplication {
 
 	public static void main(String[] args) {
