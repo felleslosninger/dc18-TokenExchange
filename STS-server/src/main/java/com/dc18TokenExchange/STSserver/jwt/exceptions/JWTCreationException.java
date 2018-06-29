3b2860128bab7dbd611071c7a2e9412bd294ab76
hellebei <40169345+hellebei@users.noreply.github.com>
2018-06-29 12:06:58 +0200

    Slettet jwt-mappen i STS

--- STS-server/src/main/java/com/dc18TokenExchange/STSserver/jwt/exceptions/JWTCreationException.java
+++
@@ -1,7 +0,0 @@
-package com.dc18TokenExchange.STSserver.jwt.exceptions;
-
-public class JWTCreationException extends RuntimeException {
-    public JWTCreationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
